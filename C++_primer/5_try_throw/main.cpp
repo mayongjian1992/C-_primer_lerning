17f9637b84961a5d89a9ed43fc742a73b3b0aba6
micheal.ma <micheal.ma@powervision.me>
2018-12-21 18:51:01 +0800

    function

--- C++_primer/5_try_throw/main.cpp
+++ C++_primer/5_try_throw/main.cpp
@@ -1,40 +1,45 @@
 /*************************************************************************
-	> File Name: main.cpp
-	> Author:micheal.ma
-	> Mail: micheal.ma@powervision.me
-	> Created Time: 2018年12月20日 星期四 17时13分55秒
- ************************************************************************/
+> File Name: main.cpp
+> Author:micheal.ma
+> Mail: micheal.ma@powervision.me
+> Created Time: 2018年12月21日 星期五 14时06分32秒
+************************************************************************/
 
 #include<iostream>
 #include "Sales_item.h"
 using namespace std;
 
-int main()
+int main(int argc,const char *argv[])
 {
-    Sales_item item1, item2;
+    Sales_item item1 , item2;
 
     while(cin >> item1 >> item2)
     {
         try
         {
-            if(item1.isbn() != item2.isbn() )
+            if(item1.isbn() != item2.isbn())
             {
-                throw runtime_error("the two item must be same");
-            }    
-            cout << item1+item2 << endl;
-        }
-        catch (runtime_error err)
-        {
-            cout << err.what() << "\nyou input maby error input again ? Y/N ?" <<endl;
-            char c;
-            cin >> c;
-            if(c=='N' || !cin)
+                throw runtime_error("the two items's isbn must be same");
+            }
+            else
             {
+                cout << item1 + item2 <<endl;
                 break;
             }
         }
-    }
+        catch(runtime_error error)
+        {
+            cout << error.what() <<"\ntry again ? [Y/N]"<<endl;
+            cout <<">>>";
+            char c;
 
+            cin >> c;
+            if(c=='N' | !cin)
+            {
+                break;    
+            }
+        }
+    }
 
     return 0;
 }
